1ff6dffd187b683dcb057fe1153c797f5769504e
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-10 14:38:58 -0600

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+@prince = Artist.create!(name: 'Prince')
+    @purple = @prince.songs.create!(title: 'Purple Rain', length: 203, play_count: 123890)
+    @raspberry = @prince.songs.create!(title: 'Raspberry Beret', length: 233, play_count: 121290)
+    
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +9,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
